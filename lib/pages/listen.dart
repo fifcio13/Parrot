3c1739d05130cfdd349deed34e130ba16c3c065e
Fifcio <filipson20@gmail.com>
2020-05-16 13:41:17 +0200

    WIP recording2

--- lib/pages/listen.dart
+++ lib/pages/listen.dart
@@ -1,53 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:parrot_flutter/components/headerNav.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-class ListenPage extends StatefulWidget {
+
+void main() => runApp(new Record());
+
+class Record extends StatefulWidget{
   @override
-  _ListenPageState createState() => _ListenPageState();
+  _RecordState createState() => _RecordState();
 }
 
-class _ListenPageState extends State<ListenPage> {
-
-  @override
-  void initState() {
-    super.initState();
-    getNamePreference().then((name){
-      setState(() {
-        if(data != null) {
-          data = name;
-        } else {
-          data = 'Activity 1';
-        }
-      });
-    });
-  }
-
-  String data = 'Activity 1';
+class _RecordState extends State<Record> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: headerNav(
-        title: data,
-      ),
-      body: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              Text('t'),
-            ],
-          ),
+      appBar: headerNav(title: 'Recording screen'),
+      body: Center(
 
-        ],
       ),
     );
   }
-}
-
-Future<String> getNamePreference() async {
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  final name = prefs.getString('name');
-
-  return name.toString();
 }
--- lib/pages/record.dart
+++ lib/pages/record.dart
@@ -1,13 +1,19 @@
+import 'dart:async';
 import 'dart:io' as io;
-import 'dart:math';
-import 'package:audio_recorder/audio_recorder.dart';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:file/file.dart';
 import 'package:file/local.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_audio_recorder/flutter_audio_recorder.dart';
 import 'package:parrot_flutter/components/headerNav.dart';
 import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  SystemChrome.setEnabledSystemUIOverlays([]);
+  return runApp(new MyApp());
+}
 
 class MyApp extends StatefulWidget {
   @override
@@ -19,87 +25,146 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: new Scaffold(
-        appBar: headerNav(
-          title: 'Record',
+        body: SafeArea(
+          child: new RecorderExample(),
         ),
-        body: AppBody(),
       ),
     );
   }
 }
 
-class AppBody extends StatefulWidget {
+class RecorderExample extends StatefulWidget {
   final LocalFileSystem localFileSystem;
 
-  AppBody({localFileSystem})
+  RecorderExample({localFileSystem})
       : this.localFileSystem = localFileSystem ?? LocalFileSystem();
 
   @override
-  State<StatefulWidget> createState() => new AppBodyState();
+  State<StatefulWidget> createState() => new RecorderExampleState();
 }
 
-class AppBodyState extends State<AppBody> {
-  Recording _recording = new Recording();
-  bool _isRecording = false;
-  Random random = new Random();
-  TextEditingController _controller = new TextEditingController();
+class RecorderExampleState extends State<RecorderExample> {
+  FlutterAudioRecorder _recorder;
+  Recording _current;
+  RecordingStatus _currentStatus = RecordingStatus.Unset;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _init();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Scaffold(
-          body: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                FlatButton(
-                  onPressed: _isRecording ? null : _start,
-                  child: Text("Start"),
-                  color: Colors.green,
-                ),
-                FlatButton(
-                  onPressed: _isRecording ? _stop : null,
-                  child: Text("Stop"),
-                  color: Colors.red,
-                ),
-                TextField(
-                  controller: _controller,
-                  decoration: new InputDecoration(
-                    hintText: 'Enter a custom path',
+    return new Scaffold(
+      appBar: headerNav(title: 'Recording'),
+      body: Padding(
+        padding: new EdgeInsets.all(8.0),
+        child: new Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              new Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: new FlatButton(
+                      onPressed: () {
+                        switch (_currentStatus) {
+                          case RecordingStatus.Initialized:
+                            {
+                              _start();
+                              break;
+                            }
+                          case RecordingStatus.Recording:
+                            {
+                              _pause();
+                              break;
+                            }
+                          case RecordingStatus.Paused:
+                            {
+                              _resume();
+                              break;
+                            }
+                          case RecordingStatus.Stopped:
+                            {
+                              _init();
+                              break;
+                            }
+                          default:
+                            break;
+                        }
+                      },
+                      child: _buildText(_currentStatus),
+                      color: Colors.lightBlue,
+                    ),
                   ),
-                ),
-                Text("File path of the record: ${_recording.path}"),
-                Text("Format: ${_recording.audioOutputFormat}"),
-                Text("Extension : ${_recording.extension}"),
-                Text(
-                    "Audio recording duration : ${_recording.duration.toString()}")
-              ]),
-        ),
+                  new FlatButton(
+                    onPressed:
+                    _currentStatus != RecordingStatus.Unset ? _stop : null,
+                    child:
+                    new Text("Stop", style: TextStyle(color: Colors.white)),
+                    color: Colors.blueAccent.withOpacity(0.5),
+                  ),
+                  SizedBox(
+                    width: 8,
+                  ),
+                  new FlatButton(
+                    onPressed: onPlayAudio,
+                    child:
+                    new Text("Play", style: TextStyle(color: Colors.white)),
+                    color: Colors.blueAccent.withOpacity(0.5),
+                  ),
+                ],
+              ),
+              new Text("Status : $_currentStatus"),
+              new Text('Avg Power: ${_current?.metering?.averagePower}'),
+              new Text('Peak Power: ${_current?.metering?.peakPower}'),
+              new Text("File path of the record: ${_current?.path}"),
+              new Text("Format: ${_current?.audioFormat}"),
+              new Text(
+                  "isMeteringEnabled: ${_current?.metering?.isMeteringEnabled}"),
+              new Text("Extension : ${_current?.extension}"),
+              new Text(
+                  "Audio recording duration : ${_current?.duration.toString()}")
+            ]),
       ),
     );
   }
 
-  _start() async {
+  _init() async {
     try {
-      if (await AudioRecorder.hasPermissions) {
-        if (_controller.text != null && _controller.text != "") {
-          String path = _controller.text;
-          if (!_controller.text.contains('/')) {
-            io.Directory appDocDirectory =
-            await getApplicationDocumentsDirectory();
-            path = appDocDirectory.path + '/' + _controller.text;
-          }
-          print("Start recording: $path");
-          await AudioRecorder.start(
-              path: path, audioOutputFormat: AudioOutputFormat.AAC);
+      if (await FlutterAudioRecorder.hasPermissions) {
+        String customPath = '/flutter_audio_recorder_';
+        io.Directory appDocDirectory;
+//        io.Directory appDocDirectory = await getApplicationDocumentsDirectory();
+        if (io.Platform.isIOS) {
+          appDocDirectory = await getApplicationDocumentsDirectory();
         } else {
-          await AudioRecorder.start();
+          appDocDirectory = await getExternalStorageDirectory();
         }
-        bool isRecording = await AudioRecorder.isRecording;
+
+        // can add extension like ".mp4" ".wav" ".m4a" ".aac"
+        customPath = appDocDirectory.path +
+            customPath +
+            DateTime.now().millisecondsSinceEpoch.toString();
+
+        // .wav <---> AudioFormat.WAV
+        // .mp4 .m4a .aac <---> AudioFormat.AAC
+        // AudioFormat is optional, if given value, will overwrite path extension when there is conflicts.
+        _recorder =
+            FlutterAudioRecorder(customPath, audioFormat: AudioFormat.WAV);
+
+        await _recorder.initialized;
+        // after initialization
+        var current = await _recorder.current(channel: 0);
+        print(current);
+        // should be "Initialized", if all working fine
         setState(() {
-          _recording = new Recording(duration: new Duration(), path: "");
-          _isRecording = isRecording;
+          _current = current;
+          _currentStatus = current.status;
+          print(_currentStatus);
         });
       } else {
         Scaffold.of(context).showSnackBar(
@@ -110,16 +175,85 @@ class AppBodyState extends State<AppBody> {
     }
   }
 
+  _start() async {
+    try {
+      await _recorder.start();
+      var recording = await _recorder.current(channel: 0);
+      setState(() {
+        _current = recording;
+      });
+
+      const tick = const Duration(milliseconds: 50);
+      new Timer.periodic(tick, (Timer t) async {
+        if (_currentStatus == RecordingStatus.Stopped) {
+          t.cancel();
+        }
+
+        var current = await _recorder.current(channel: 0);
+        // print(current.status);
+        setState(() {
+          _current = current;
+          _currentStatus = _current.status;
+        });
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _resume() async {
+    await _recorder.resume();
+    setState(() {});
+  }
+
+  _pause() async {
+    await _recorder.pause();
+    setState(() {});
+  }
+
   _stop() async {
-    var recording = await AudioRecorder.stop();
-    print("Stop recording: ${recording.path}");
-    bool isRecording = await AudioRecorder.isRecording;
-    File file = widget.localFileSystem.file(recording.path);
-    print("  File length: ${await file.length()}");
+    var result = await _recorder.stop();
+    print("Stop recording: ${result.path}");
+    print("Stop recording: ${result.duration}");
+    File file = widget.localFileSystem.file(result.path);
+    print("File length: ${await file.length()}");
     setState(() {
-      _recording = recording;
-      _isRecording = isRecording;
+      _current = result;
+      _currentStatus = _current.status;
     });
-    _controller.text = recording.path;
+  }
+
+  Widget _buildText(RecordingStatus status) {
+    var text = "";
+    switch (_currentStatus) {
+      case RecordingStatus.Initialized:
+        {
+          text = 'Start';
+          break;
+        }
+      case RecordingStatus.Recording:
+        {
+          text = 'Pause';
+          break;
+        }
+      case RecordingStatus.Paused:
+        {
+          text = 'Resume';
+          break;
+        }
+      case RecordingStatus.Stopped:
+        {
+          text = 'Init';
+          break;
+        }
+      default:
+        break;
+    }
+    return Text(text, style: TextStyle(color: Colors.white));
+  }
+
+  void onPlayAudio() async {
+    AudioPlayer audioPlayer = AudioPlayer();
+    await audioPlayer.play(_current.path, isLocal: true);
   }
 }
